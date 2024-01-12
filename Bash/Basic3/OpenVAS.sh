9c8f781f383d814fa0dcba1ec14662fd84aeb8e3
opsboltz <63130259+opsboltz@users.noreply.github.com>
2024-01-12 18:40:38 -0500

    OpenVAS script update

--- Bash/Basic3/OpenVAS.sh
+++ Bash/Basic3/OpenVAS.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Update package information
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 # Install OpenVAS and Lynis
 sudo apt install -y openvas
 
 echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
 wget -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add -
-sudo apt update
-sudo apt install lynis -y
+apt update
+apt install lynis -y
 
 lynis show version
 sleep 1
@@ -36,7 +36,7 @@ sleep 600
 openvasmd --get-report $scanner_id --format pdf > openvas_results.pdf
 
 # Clean up temporary files
-sudo openvasmd --delete-scanner=$scanner_id
+openvasmd --delete-scanner=$scanner_id
 rm -f /var/run/openvassd.sock
 
 # All done!
